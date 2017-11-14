293ff0124dff80a1b079f013ce30aa2329847436
reivaz <TheSpamBoite+reivaz@gmail.com>
2017-11-14 17:19:01 +0000

    Setup Junit

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class Main {
+
+	/**
+	 * Entry point of the project. args is supposed to be a filepath
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
+	
+}
---
+++ src/test/java/com/example/RobotTest.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class RobotTest {
+
+	@Test
+	public void testConstructor(){		
+		Assert.assertTrue(true);
+	}
+	
+}
